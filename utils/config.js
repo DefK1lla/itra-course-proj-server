56e461e29449713ea899a61034f40440369cbb52
defk1lla <set.adoyan@gmail.com>
2022-07-10 19:36:00 +0400

    qewqeqw

--- utils/config.js
+++ utils/config.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 module.exports = {
    DB_URI: process.env.DB_URI,
-   PORT: process.env.PORT || 5000,
+   PORT: process.env.PORT,
    SECRET_KEY: process.env.SECRET_KEY,
    MY_AWS_BUCKET_NAME: process.env.MY_BUCKET_NAME, 
    MY_AWS_BUCKET_REGION: process.env.MY_BUCKET_REG, 
